68c1ce9130feb333a8bd3d72ead0814ba3134c98
ffrankoluka <fran.garcia.luca@gmail.com>
2026-08-17 17:56:23 -0300

    .env

--- APP/main.py
+++ APP/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import usuarios,lenguajes,auth
+from APP.routers import usuarios,lenguajes,auth
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
